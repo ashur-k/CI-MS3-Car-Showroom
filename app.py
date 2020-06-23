9cbc6df473a6b2da6277206615cafe8ce556a30f
ashur-k <ashurkanwal@yahoo.com>
2020-06-23 14:56:39 +0000

    in process of desgning car info form

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mySecret'
 
 app.config["MONGO_DBNAME"] = 'Cars_Sales_Showroom'
 app.config["MONGO_URI"] = os.getenv("MONGO_URI", 'mongodb://localhost')
@@ -20,23 +21,32 @@ cars_specs = mongo.db.car_specs_perfomance_economy
 cars_summary = mongo.db.car_summary
 cars_info = mongo.db.cars
 cars_running_cost = mongo.db.running_cost
+cars_features = mongo.db.car_features
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', cars=cars_info.find())
+    car_features = cars_features.find()
+    return render_template('index.html',  cars_make=cars_make.find(), car_features = car_features)
 
 
-@app.route('/trial')
-def trial():
-    return render_template('trial.html', cars=cars_summary.find())
+@app.route('/add_new_car')
+def add_new_car():
+    car_makers = cars_make.find()
+    return render_template('add_new_car.html', car_makers=car_makers)
 
 
 @app.route('/view_car/<car_registration>')
 def view_car(car_registration):
-    car_info = cars_info.find({'registration_number': car_registration})
-    c_summary = cars_summary.find({'registration_number': car_registration})
-    return render_template('view_car.html', cars=car_info, summary=c_summary)
+    
+    return render_template('view_car.html', )
+
+
+def helper_function():
+    car_makers = cars_make.find()
+    for makers in car_makers:
+        print(makers.car_make)
+
 
 
 if __name__ == '__main__':
